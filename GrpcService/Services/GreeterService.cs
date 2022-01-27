a81f51daf9bdc85863c51b240c3816da2fa486f9
Vikash Tripathi <tripvik@gmail.com>
2022-01-27 17:36:40 -0500

    Add project files.

---
+++ GrpcService/Services/GreeterService.cs
@@ -0,0 +1,23 @@
+using Grpc.Core;
+
+namespace GrpcService.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Id = 15,
+                FirstName = "Steve",
+                Value = 25.999m
+            });
+        }
+    }
+}
